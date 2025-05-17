c3ee511c53ebff5fc3fb137bb6a4826207609521
Htoo Myat Nyi Nyi <htoomyatnyinyi@MacBook-Pro.local>
2025-05-17 16:53:13 +0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="h-screen bg-amber-200">
+      <h1>This is REact Front Edn</h1>
+    </div>
+  );
+};
+
+export default App;
